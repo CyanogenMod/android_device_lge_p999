c749e0fdcbff9f0e85a0d4bbf475d32fc2305214
Chris Soyars <ctso@ctso.me>
2011-04-22 23:05:53 -0400

    Split off extract-files/setup-makefiles/wireless.ko from common.

--- p999.mk
+++ p999.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, device/lge/star-common/star.mk)
 
+# Inherit non-open-source blobs.
+$(call inherit-product-if-exists, vendor/lge/p999/p999-vendor.mk)
+
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/lge/p999/kernel
 else
@@ -14,7 +17,8 @@ PRODUCT_COPY_FILES += \
     device/lge/p999/vold.fstab:system/etc/vold.fstab \
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+    $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/prebuilt/wireless.ko:system/lib/modules/wireless.ko
 
 PRODUCT_NAME := lge_p999
 PRODUCT_DEVICE := p999
